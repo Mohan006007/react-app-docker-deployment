a8168abb6fbd7713ad98ef13263500c3da767977
MohanaKrishnan <mohanakrishnan707@gmail.com>
2025-01-20 03:21:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /home/ubuntu/devops-build/build
-
 echo "Starting Docker containers"
+docker-compose down
 docker-compose up -d
 
 if [ $? -eq 0 ]; then
     echo "Docker containers started successfully"
 else
     echo "Docker containers failed to start"
+    docker-compose logs
     exit 1
 fi
